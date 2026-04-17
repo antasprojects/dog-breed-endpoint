1793bb78877e78af847194d9f24da025961e42d8
antasprojects <asatki@gmail.com>
2026-04-17 11:05:06 +0100

    first commit

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,3 @@
 from pydantic import BaseModel
 from typing import List
 
-class TopKItem(BaseModel):
-    label: str
-    prob: float
-
-class PredictResponse(BaseModel):
-    breed: str
-    confidence: float
-    top_k: List[TopKItem]
